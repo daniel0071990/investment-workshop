297d7efdb6befd3ec793c67506a8631dfc9e7207
daniel0071990 <daniel@timeline.co>
2026-03-30 20:52:04 +0100

    Add files via upload

--- src/firebase.js
+++ src/firebase.js
@@ -8,7 +8,7 @@ import { getDatabase, ref, push, set, get, remove, onValue } from "firebase/data
 const firebaseConfig = {
   apiKey: "AIzaSyA_49WDIDT7MkRqv5TFsDTx9Q_xj-JyChk",
   authDomain: "investment-team-workshop-1.firebaseapp.com",
-  databaseURL: https://investment-team-workshop-1-default-rtdb.europe-west1.firebasedatabase.app",
+  databaseURL: "https://investment-team-workshop-1-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "investment-team-workshop-1",
   storageBucket: "investment-team-workshop-1.firebasestorage.app",
   messagingSenderId: "1:1048724444277:web:f8f6b7ba0b7575d16cd43c",
